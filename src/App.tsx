0ad4bc73afe4dadd9315cb28b987230242f5f51f
Yurii Dano <yuriidano@gmail.com>
2025-05-27 23:21:39 +0200

    basic css settings are set. Header component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Header } from './components/Header/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
+ const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+     return (
+         <div className='flex flex-col min-h-full overflow-hidden'>
+             <div className='container'>
+                <Header />
+                 <main className='flex-grow-1 flex-shrink-1 flex-basis-full'>
+
+                 </main>
+             </div>
+         </div>
+     )
+ }
 
 export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import logo from '../../assets/icons/logo.svg'
+import basket from '../../assets/icons/basket.svg'
+import classNames from 'classnames'
+import stylles from './Header.module.scss'
+
+
+export const Header = () => {
+
+    return (
+        <header className="!flex !items-center justify-between min-h-38">
+            <div className='flex gap-x-3 items-center'>
+                <div>
+                    <img className='max-w-full' src={logo} alt="logo" />
+                </div>
+                <div>
+                    <div className='text-2xl font-extrabold '>REACT PIZZA</div>
+                    <div className='text-my-gray-text'>the most delicious pizza in the universe</div>
+                </div>
+            </div>
+            <div className={classNames('min-h-12.5 bg-my-orange inline-flex gap-x-7 !px-7 rounded-3xl', stylles.button)}>
+                    <div className='flex gap-x-2 text-white font-bold items-center'>
+                        <span>520</span>
+                        <span>₽</span>
+                    </div>
+                    <div className='flex gap-x-2 items-center'>
+                        <div>
+                            <img src={basket} alt="basket" />
+                        </div>
+                        <div className='text-white font-bold'>3</div>
+                    </div>
+            </div>
+        </header>
+    )
+}
